7097c317f82e4f3670e29a1839463d776316c5ff
Rushali Asthana <rushaliasthana@gmail.com>
2020-08-18 00:01:15 +0530

    Updates

---
+++ precache-manifest.616294fe847a114bcc0d8700d749707b.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dd403a8cb75412a35d71033386b8b81",
+    "url": "/RevisedLoginSignup/index.html"
+  },
+  {
+    "revision": "5bacde4a0d45ded00de8",
+    "url": "/RevisedLoginSignup/static/css/main.df45f16d.chunk.css"
+  },
+  {
+    "revision": "ef61141713bd4c1bae0e",
+    "url": "/RevisedLoginSignup/static/js/2.7d33e18c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RevisedLoginSignup/static/js/2.7d33e18c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bacde4a0d45ded00de8",
+    "url": "/RevisedLoginSignup/static/js/main.9e3f5115.chunk.js"
+  },
+  {
+    "revision": "88572271ea4d15c57012",
+    "url": "/RevisedLoginSignup/static/js/runtime-main.5829caa9.js"
+  },
+  {
+    "revision": "f222224d59acc4c819b5870af9a0aa99",
+    "url": "/RevisedLoginSignup/static/media/Bg.f222224d.png"
+  },
+  {
+    "revision": "4addc00a905284b584cef75fb8962682",
+    "url": "/RevisedLoginSignup/static/media/Facebook.4addc00a.png"
+  },
+  {
+    "revision": "e50b88a7fb20ded51b474130d787f127",
+    "url": "/RevisedLoginSignup/static/media/Google.e50b88a7.png"
+  },
+  {
+    "revision": "948c8377a44d7159e44c997c73a2d5f1",
+    "url": "/RevisedLoginSignup/static/media/Googlewhite.948c8377.png"
+  },
+  {
+    "revision": "9082b0a610a97ac865b06a95eb7ab874",
+    "url": "/RevisedLoginSignup/static/media/Loginmobile.9082b0a6.png"
+  },
+  {
+    "revision": "2cfbe329d6733537e980d407bb7483b9",
+    "url": "/RevisedLoginSignup/static/media/SignUpmobile.2cfbe329.png"
+  },
+  {
+    "revision": "75c51b3e9c70fdc8c49cff79e00fb907",
+    "url": "/RevisedLoginSignup/static/media/Signupbackground.75c51b3e.png"
+  },
+  {
+    "revision": "fb1cfbae164d6074ac01f9e7f92be69c",
+    "url": "/RevisedLoginSignup/static/media/appstore.fb1cfbae.png"
+  },
+  {
+    "revision": "223a06a58aabb8a4e114a262d3d41fbf",
+    "url": "/RevisedLoginSignup/static/media/fbwhite.223a06a5.png"
+  },
+  {
+    "revision": "986c8fb70033c32144d51ca82f423f5a",
+    "url": "/RevisedLoginSignup/static/media/googleplay.986c8fb7.png"
+  },
+  {
+    "revision": "ddb13ccd928ac660665bbf5b0aef1744",
+    "url": "/RevisedLoginSignup/static/media/illustration.ddb13ccd.png"
+  },
+  {
+    "revision": "68e51e59d85dc1ec7723f85e1f12b349",
+    "url": "/RevisedLoginSignup/static/media/illustration2.68e51e59.png"
+  },
+  {
+    "revision": "ae492379294830f9065fc09b5cd4a9b0",
+    "url": "/RevisedLoginSignup/static/media/illustration3.ae492379.png"
+  },
+  {
+    "revision": "c68547da19a56eda6a458c9effe26508",
+    "url": "/RevisedLoginSignup/static/media/logo.c68547da.png"
+  },
+  {
+    "revision": "0e6c8213fd9e29f221b100321c48a9fa",
+    "url": "/RevisedLoginSignup/static/media/logo1.0e6c8213.png"
+  },
+  {
+    "revision": "414e6e4946c57969122bfee36dd610ce",
+    "url": "/RevisedLoginSignup/static/media/welcome.414e6e49.png"
+  }
+]);
